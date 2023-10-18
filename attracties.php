4fd1b583705537b0a7467f6c305d44ed6f7daba3
anouar sealioui <2111910@talnet.nl>
2023-10-18 13:47:59 +0200

    Style toegevoegd, attracties bijgewerkt

---
+++ attracties.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html lang="nl">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>magic</title>
+
+    <head>
+        <style>
+            .image-container {
+                position: absolute;
+                top:20;
+                left:20;
+            }
+        </style>
+    </head>
+<div class="image-container">
+    <img src="/images/logo%20magic.png" alt="Your Image" width="200" height="200">
+</div>
+    <head>
+        <title>magic</title>
+        <link rel="stylesheet" type="text/css" href="attracties.css">
+    </head>
+    <body>
+    <header>
+        <h1>magic</h1>
+        <div class="search-bar">
+            <form action="search.php" method="get">
+                <input type="text" name="search" placeholder="Search...">
+                <input type="submit" value="Search">
+            </form>
+        </div>
+    </header>
