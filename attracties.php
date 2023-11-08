a1d7c2c9411789592e254bba27bd83b48d36045f
anouar sealioui <2111910@talnet.nl>
2023-11-08 11:17:19 +0100

    Style toegevoegd, attracties bijgewerkt

--- attracties.php
+++ attracties.php
@@ -5,31 +5,64 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>magic</title>
+    <title>magic thrills</title>
+    <link rel="stylesheet" type="text/css" href="attracties.css">
+</head>
+<body>
 
-    <head>
-        <style>
-            .image-container {
-                position: absolute;
-                top:20;
-                left:20;
-            }
-        </style>
-    </head>
-<div class="image-container">
-    <img src="/images/logo%20magic.png" alt="Your Image" width="200" height="200">
-</div>
-    <head>
-        <title>magic</title>
-        <link rel="stylesheet" type="text/css" href="attracties.css">
-    </head>
-    <body>
-    <header>
-        <h1>magic</h1>
-        <div class="search-bar">
-            <form action="search.php" method="get">
-                <input type="text" name="search" placeholder="Search...">
-                <input type="submit" value="Search">
-            </form>
+<header>
+    <div class="image-container">
+        <img src="/images/logo%20magic.png" alt="Your Image" width="200" height="200">
+    </div>
+    <h1>magicthrills</h1>
+    <div class="search-bar">
+        <form action="search.php" method="get">
+            <input type="text" name="search" placeholder="Search...">
+            <input type="submit" value="Search">
+        </form>
+    </div>
+</header>
+
+<ul class="nav">
+    <li><a href="#">Home</a></li>
+    <li><a href="#">About us</a></li>
+    <li><a href="#">atracties</a></li>
+    <li><a href="#">Contact</a></li>
+</ul>
+
+<main>
+    <section class="sidebar">
+        <ul class="attracties">
+            <li><a href="#">de draaikolk</a></li>
+            <li><a href="#">de torenval</a></li>
+            <li><a href="#">de bliksemrit</a></li>
+            <li><a href="#">de avonturenberg</a></li>
+            <li><a href="#">de wilwaterbaam</a></li>
+            <li><a href="#">de spookhuis</a></li>
+            <li><a href="#">de achtbaan van de draak</a></li>
+            <li><a href="#">de ruimtereis</a></li>
+            <li><a href="#">het reuzenrad</a></li>
+            <li><a href="#">het jungleexpeditie</a></li>
+        </ul>
+    </section>
+    <div class="image-container2">
+        <img src="/images/logo%20magic.png" alt="Your Image" width="200" height="200">
+    </div>
+    <section class="content">
+        <div class="pretpark-summary">
+            <h3>Welkom bij Ons Pretpark</h3>
+            <p>Ontdek een wereld vol opwinding en avontuur in Ons Pretpark! Geniet van spannende attracties, heerlijk eten,
+                en onvergetelijke herinneringen voor het hele gezin.De Efteling is een betoverend pretpark in Nederland,
+                bekend om zijn sprookjesachtige sfeer.
+                De Droomvlucht neemt bezoekers mee op een magische reis door een betoverd bos met elfjes en mythische
+                wezens.
+                Baron 1898 is een angstaanjagende duikachtbaan die je naar de diepten van een oude mijn voert.
+                Symbolica is een paleis vol wonderen, waar je de geheimen van koning Pardulfus kunt ontdekken.
+                Joris en de Draak biedt een spannende race-ervaring op houten achtbanen,he reis door Oosterse markten en
+                mysterieuze paleizen.
+                Vogel Rok is een indoor achtbaan die je meeneemt op een duizelingwekkende vlucht door de wereld van
+                exotische vogels.
         </div>
-    </header>
+    </section>
+</main>
+</body>
