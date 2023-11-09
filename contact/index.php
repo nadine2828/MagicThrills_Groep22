da5c0ab0707321612267862937377ffd38a70cf2
Remy <2093296@talnet.nl>
2023-11-09 11:22:16 +0100

    Style CSS toegevoegd, en pagina opgebouwd.

--- contact/index.php
+++ contact/index.php
@@ -1,13 +1,61 @@
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Remy Gurpal Singh - Theme Park Business magic thrills</title>
+    <link rel="stylesheet" href="../stylesheets/remy.css">
 </head>
 <body>
+<header>
+    <div class="container">
+        <div id="branding">
+            <h1><a href="index.html">Remy Gurpal Singh</a></h1>
+        </div>
+        <nav>
+            <ul>
+                <li class="current"><a href="#showcase">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    </div>
+</header>
+
+<section id="showcase">
+    <img class="logo" src="../images/LogoMagicThrills.jpg" alt="Remy's Theme Park Logo">
+    <h1>Welcome to Remy's Theme Park Business</h1>
+    <p>Crafting unforgettable experiences since 2023</p>
+</section>
+
+<section id="about" class="container">
+    <h2>About Remy</h2>
+    <p>I am Remy Gurpal Singh, born on October 20. As a seasoned professional in the theme park industry, I specialize in creating and managing top-tier entertainment experiences. With a passion for excellence and a dedication to creating joyous memories, my work has been recognized by industry leaders and guests alike.</p>
+</section>
+
+<section id="contact" class="contact-section">
+    <div class="container">
+        <h2>Contact Remy</h2>
+        <form class="contact-form" action="MAILTO:2093296@talnet.nl" method="post" enctype="text/plain">
+            <label for="name">Name</label>
+            <input type="text" id="name" name="name" required>
+
+            <label for="email">Email</label>
+            <input type="email" id="email" name="email" required>
+
+            <label for="message">Message</label>
+            <textarea id="message" name="message" required></textarea>
+
+            <button type="submit">Send Message</button>
+        </form>
+    </div>
+</section>
+
+<footer>
+    <p>Remy's Theme Park Business &copy; 2023</p>
+</footer>
 
 </body>
 </html>
+
+
